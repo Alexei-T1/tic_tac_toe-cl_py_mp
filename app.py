ec82ad7f3543a8a6519b9c531b424d6bf4104524
Alexei T <turencko.alexei@yandex.ru>
2026-05-24 18:46:09 +0400

    draw field function for CL

---
+++ app.py
@@ -0,0 +1,13 @@
+from draw_field import draw_field
+
+field = [[None, None, None] for i in range(3)]
+
+GAMER_F = "X"
+GAMER_S = "O"
+GAME_END = False
+
+
+draw_field(field) 
+
+# while not GAME_END:
+#     pass
