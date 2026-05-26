cb805f7a8e600cc65c15d4c43499db0c132fbb9e
Alexei T <turencko.alexei@yandex.ru>
2026-05-26 22:49:52 +0400

    refactor input function gamer

--- check_.py
+++ check_.py
@@ -9,12 +9,14 @@ WRONG_INPUT ='wrong_input'
 def check_field(field):
     return field
 
-def check_step(step, field):
-    x, y = step.split(',')
+def check_step(step, field):   
     try:
+        x, y = step.split(',')
         x, y = int(x), int(y)
     except:
         return WRONG_INPUT
     return USED_FIELD if field[y][x] == GAMER_F or field[y][x] == GAMER_S else (x,y)
-    
+
+
+  
 
--- input.py
+++ input.py
@@ -1,3 +1,5 @@
+from check_ import check_step
+
 GAMER_F = "X"
 GAMER_S = "O"
 USED_FIELD = 'used_field'
@@ -7,3 +9,20 @@ def input_o(field):
     cords = [i for st in field for i in st].count(GAMER_F)
     return cords
 
+def input_step(field, gamer_):  
+
+    step_ = input(f"input {gamer_} x, y=  ")
+    result_check = check_step(step_, field)
+
+    while result_check == WRONG_INPUT or result_check == USED_FIELD:
+
+        if result_check == WRONG_INPUT:
+            print(f"wrong input, use number x(1,2,3),y(1,2,3)")
+            
+        elif result_check == USED_FIELD:
+            print(f"field used, try new field")      
+
+        step_ = input(f"input {gamer_} x, y=  ")
+        result_check = check_step(step_, field)
+    
+    return result_check
