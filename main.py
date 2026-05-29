866baa19afce3721e6d9754d9abcc822d8db3ec9
Alexei T <turencko.alexei@yandex.ru>
2026-05-29 23:49:44 +0400

    fix another game repeat ('N', 'n', 'Y', 'y')

--- main.py
+++ main.py
@@ -4,17 +4,13 @@ import app
 
 if __name__ == "__main__":
 
-    repeat = True
 
-    while repeat:
+    while True:
         app.game()
         repeat = input('another game? input: Y/N  ')
 
-        while repeat != 'N' != 'n' != 'Y' != 'y':
-            if repeat == 'N' or repeat == 'n':
-                repeat = False
-                break
-            if repeat == 'Y' or repeat == 'y':
-               repeat = True
-               break
-            repeat = input('another game? input: Y/N  ') 
+        while repeat not in ('N', 'n', 'Y', 'y'):
+           repeat = input('another game? input: Y/N  ')
+
+        if repeat in ('N', 'n'):
+            break      
